29b153401f65ea47578a8bd3b7055cc42fa85277
arksouthern <169290859+arksouthern@users.noreply.github.com>
2025-01-31 12:34:44 -0600

    Luna branch init

--- frontend/src/lib/hx.tsx
+++
@@ -1,5 +0,0 @@
-import { createComponent } from "solid-js"
-
-export function Handlers<T extends Record<string, (a: any) => any>>(props: {handlers: T, children: (p: T) => any}): any {
-    return <>{createComponent(props.children, props.handlers)}</>
-}
--- frontend/src/lib/vx.tsx
+++
@@ -1,5 +0,0 @@
-import { createComponent } from "solid-js";
-
-export function Variables<T extends Record<string, any>>(props: T & {children: (p: Omit<T, "children">) => any}): any {
-    return createComponent(props.children, props)
-}
